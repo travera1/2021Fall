db7f0f6d662773f8a2cc762a051a653c29cb2072
Aaron Traver <travera1@hawkmail.newpaltz.edu>
2021-09-27 14:02:17 -0400

    using express for applicaiton

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+.get('/newpaltz', (req, res) => {
+  res.send('hello New paltz')
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
